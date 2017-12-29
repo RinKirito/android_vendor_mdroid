a55e6ff404c14c3f9ec5ead8e6b657ffde350085
Lakshya Taneja <taneja.lakshay5@gmail.com>
2017-12-29 18:07:09 +0530

    soong: squash soong commits
    Commits:
    9dd542a6912861eab1c7a10451074da99cdf013e
    39c82b0fb8ca521064d9a251a63426cb60f5d448
    8726bb6bedf6d8e2e6d54afb47deb93200f3fd33

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,15 +1,53 @@
 package android
 type Product_variables struct {
+	Has_legacy_camera_hal1 struct {
+		Cflags []string
+	}
+
+	Uses_media_extensions struct {
+		Cflags []string
+	}
+
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
+
+	Mtk_hardware struct {
+		Cflags []string
+	}
+
+	Cant_reallocate_omx_buffers struct {
+		Cflags []string
+	}
+
+	Qcom_bsp_legacy struct {
+		Cflags []string
+	}
+
+	Qti_flac_decoder struct {
+		Cflags []string
+	}
+	Use_legacy_rescaling struct {
+		Cflags []string
+	}
+
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
 }
 
 type ProductVariables struct {
+	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Uses_media_extensions   *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
+	Mtk_hardware            *bool `json:",omitempty"`
+	QTIAudioPath            *string `json:",omitempty"`
+	QTIDisplayPath          *string `json:",omitempty"`
+	QTIMediaPath            *string `json:",omitempty"`
+	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
+	Qcom_bsp_legacy         *bool `json:",omitempty"`
+	Qti_flac_decoder        *bool `json:",omitempty"`
+	Use_legacy_rescaling    *bool `json:",omitempty"`
 }
