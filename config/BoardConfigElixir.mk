55d5884e7eb6b84d4cf569ea66d363620fd36223
Michael Bestas <mkbestas@lineageos.org>
2018-03-21 18:29:45 +0200

    elixir: Set TARGET_CRYPTFS_HW_PATH if HW crypto is enabled
    * So TWRP can use it
    
    Change-Id: Ibc1d9d943d6d4e4e5248f623cd06782335f8ae98
    Signed-off-by: LakshyaTaneja <taneja.lakshay5@gmail.com>

--- config/BoardConfigElixir.mk
+++ config/BoardConfigElixir.mk
@@ -2,3 +2,8 @@
 #ifeq ($(WITH_CM_CHARGER),true)
 #    BOARD_HAL_STATIC_LIBRARIES := libhealthd.cm
 #endif
+
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+endif
