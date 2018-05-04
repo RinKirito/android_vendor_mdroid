d27a3b5c93b39c974f3713e9bd07c0e46b033625
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2018-05-04 12:19:44 +0300

    soong: Fix MDroid variables

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -2,7 +2,7 @@ mdroid_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-	echo '"mdroid": {'; \
+	echo '"Mdroid": {'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
 	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
