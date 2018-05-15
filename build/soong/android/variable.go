5b637bbf1eccf72c12d15063af47d370035e5c39
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2018-05-15 17:55:13 +0300

    Fix derp in var for linker shim libs

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -52,5 +52,6 @@ type ProductVariables struct {
 	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
 	Use_legacy_rescaling    *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
+	Target_shim_libs *string `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
 }
